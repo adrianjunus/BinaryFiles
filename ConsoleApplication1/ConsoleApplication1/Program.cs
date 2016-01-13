a72ef6535056ca69bff5aefa98d0885fc5cb53d1
Adrian Junus <adrian.junus@milliman.com>
2016-01-13 15:19:34 -0800

    First Commit
    Practice writing/reading file streams. Need to find out how to "get
    access" to file paths when creating files.

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace StringKata
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("File Path : ");
+            string myFile = Console.ReadLine();
+            if (!File.Exists(myFile + "TextFile.txt"))
+            {
+                // Create file and put some text in it.
+                using (StreamWriter sw = File.CreateText(@myFile))
+                {
+                    sw.WriteLine("Hello");
+                    sw.WriteLine("And Welcome");
+                }
+
+            }
+        }
+    }
+}
